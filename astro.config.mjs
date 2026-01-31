97323f4f27b26ce82b1ae0ae2b012949f0fb24e1
jfuertes8 <jfuertes8@gmail.com>
2026-01-31 17:26:02 +0100

    eliminar la propiedad base de la configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jfuertes8.github.io',
-  base: '/portfolio',
   vite: {
     plugins: [tailwindcss()]
   }
